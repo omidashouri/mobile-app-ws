66ddfb49205a427f16ab120922b622ad09d801a2
o.ashouri <omidashouri@gmail.com>
2019-10-12 16:09:15 +0330

    Initial commit : omidashouri : 98.07.20

---
+++ src/main/java/ir/omidashouri/mobileappws/controller/v1/UserController.java
@@ -0,0 +1,11 @@
+package ir.omidashouri.mobileappws.controller.v1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users/v1") //http:localhost:8080/users/v1
+public class UserController {
+
+
+}
