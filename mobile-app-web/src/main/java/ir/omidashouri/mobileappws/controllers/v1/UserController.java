8bb516db5d769f00a49a07000b23d265ca080e4f
Omid Ashouri <omidashouri@gmail.com>
2019-11-09 00:26:50 +0330

    11.2. Getting Response in XML or JSON format : omidashouri : 98.08.18

--- mobile-app-web/src/main/java/ir/omidashouri/mobileappws/controllers/v1/UserController.java
+++ mobile-app-web/src/main/java/ir/omidashouri/mobileappws/controllers/v1/UserController.java
@@ -6,6 +6,7 @@ import ir.omidashouri.mobileappws.models.response.UserRest;
 import ir.omidashouri.mobileappws.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,8 +17,11 @@ public class UserController {
 
     private final UserService userService;
 
-    //http://localhost:8080/users/v1/aLIRVt88hdQ858q5AMURm1QI6DC3Je
-    @GetMapping(path = "/{userPublicId}")
+    // http://localhost:8080/users/v1/aLIRVt88hdQ858q5AMURm1QI6DC3Je
+    // in header add Accept : application/xml or application/json
+    @GetMapping(path = "/{userPublicId}",
+//            make response as XML or JSON
+            produces = {MediaType.APPLICATION_JSON_VALUE , MediaType.APPLICATION_XML_VALUE})
     public UserRest getUser(@PathVariable String userPublicId){
 
         UserRest returnValue = new UserRest();
@@ -29,7 +33,10 @@ public class UserController {
         return returnValue;
     }
 
-    @PostMapping
+    @PostMapping(
+//            consume for accepting XML or jason in RequestBody
+             consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.APPLICATION_XML_VALUE}
+            ,produces = {MediaType.APPLICATION_JSON_VALUE , MediaType.APPLICATION_XML_VALUE})
     public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails){
 
         UserRest returnValue = new UserRest();
