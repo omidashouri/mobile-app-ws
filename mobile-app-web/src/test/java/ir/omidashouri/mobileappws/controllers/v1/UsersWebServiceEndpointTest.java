dc43273c142838738f4386a338a1e8b872339030
Omid Ashouri <omidashouri@gmail.com>
2020-01-06 22:58:16 +0330

    26.21. Delete User Details API Call Run Test Method : omidashouri : 98.09.16

--- mobile-app-web/src/test/java/ir/omidashouri/mobileappws/controllers/v1/UsersWebServiceEndpointTest.java
+++ mobile-app-web/src/test/java/ir/omidashouri/mobileappws/controllers/v1/UsersWebServiceEndpointTest.java
@@ -6,6 +6,7 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Assert;
 import org.junit.FixMethodOrder;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runners.MethodSorters;
@@ -20,13 +21,14 @@ import static io.restassured.RestAssured.given;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class UsersWebServiceEndpointTest {
 
-//    for running this class first start application then execute 'userLoginTest'
+//    for running this class first start application then execute this class
 
     private static final String USER_EMAIL_ADDRESS = "omidashouri@gmail.com";
     private static final String USER_PASSWORD = "123";
 
     private static String authorizationHeader;
     private static String  userPublicId;
+    private static List<Map<String,String>> addresses;
 
     @BeforeEach
     void setUp() {
@@ -87,15 +89,15 @@ public class UsersWebServiceEndpointTest {
 //        get value from response
         String userPublicId = response.jsonPath().getString("userPublicId");
         String userEmailAddress = response.jsonPath().getString("email");
-        String userFirstNAme = response.jsonPath().getString("firstName");
+        String userFirstName = response.jsonPath().getString("firstName");
         String userLastName = response.jsonPath().getString("lastName");
-        List<Map<String,String>> addresses = response.jsonPath().getList("addresses");
+        addresses = response.jsonPath().getList("addresses");
 //        comment line bellow because addresses is null (fill it later)
 //        String addressPublicID = addresses.get(0).get("addressPublicId");
 
         Assert.assertNotNull(userPublicId);
         Assert.assertNotNull(userEmailAddress);
-        Assert.assertNotNull(userFirstNAme);
+        Assert.assertNotNull(userFirstName);
         Assert.assertNotNull(userLastName);
 
         Assert.assertEquals(USER_EMAIL_ADDRESS,userEmailAddress);
@@ -104,6 +106,64 @@ public class UsersWebServiceEndpointTest {
 //        Assert.assertTrue(addressPublicID.length() == 32);
     }
 
+    /**
+     * updateUserTest
+     */
+    @Test
+    final void c(){
+        Map<String,Object> userUpdating = new HashMap<>();
+        userUpdating.put("firstName","omidTest");
+        userUpdating.put("lastName","ashouriTest");
 
+        Response response = given()
+                                .contentType(ContentType.JSON)
+                                .accept(ContentType.JSON)
+                .header("Authorization",authorizationHeader)
+                .pathParam("userPubId",userPublicId)
+                .body(userUpdating)
+                .when()
+                .put("/v1/users/{userPubId}")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .contentType(ContentType.JSON)
+                .extract()
+                .response();
+
+        String userFirstName = response.jsonPath().getString("firstName");
+        String userLastName = response.jsonPath().getString("lastName");
+        List<Map<String,String>> addressesSaved = response.jsonPath().getList("addresses");
+
+        Assertions.assertEquals("omidTest",userFirstName);
+        Assertions.assertEquals("ashouriTest",userLastName);
+
+//        addressesSaved is null, fill it later
+/*        Assert.assertNotNull(addressesSaved);
+        Assert.assertTrue(addresses.size() == addressesSaved.size());
+        Assertions.assertEquals(addresses.get(0).get("streetName"),addressesSaved.get(0).get("streetName"));*/
+    }
+
+    /**
+     * userDeleteTest
+     */
+//    @Ignore    use to ignore a test
+    @Test
+    final void d(){
+
+        Response response = given()
+                .accept(ContentType.JSON)
+                .header("Authorization",authorizationHeader)
+                .pathParam("userPubId",userPublicId)
+                .when()
+                .delete("/v1/users/{userPubId}")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .contentType(ContentType.JSON)
+                .extract()
+                .response();
+
+        String returnOperationResult = response.jsonPath().getString("operationResult");
+
+        Assertions.assertEquals("SUCCESS",returnOperationResult);
+    }
 
 }
